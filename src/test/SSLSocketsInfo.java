637d9dcd440856892bb7320424030ff7af26f72b
julian <julian.gregorio@adhocware.com>
2019-05-03 13:26:06 +0200

    update

--- src/test/SSLSocketsInfo.java
+++ src/test/SSLSocketsInfo.java
@@ -1,5 +1,11 @@
 package test;
 
+import java.security.Provider;
+import java.security.Provider.Service;
+import java.security.Security;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author julian
@@ -30,6 +36,30 @@ public class SSLSocketsInfo {
     CLIENT: javax.net.ssl.keyStore=clientKeystore.jks
     SERVER: javax.net.ssl.trustStore=serverTruststore.jks
     
-    */
-    
+     */
+    public static void main(String[] args) {
+        
+        showSecurityInfo();
+    }
+
+    private static void showSecurityInfo() {
+        
+        Set<String> types = new HashSet<>();
+        
+        Provider[] providerList = Security.getProviders();
+        for (Provider provider : providerList) {
+            System.out.println(new String(new char[80]).replace('\0', '='));
+            System.out.println("Name: " + provider.getName());
+            System.out.println("Class: " + provider.getClass());
+            System.out.println("Information:\n" + provider.getInfo());
+            System.out.println(new String(new char[80]).replace('\0', '-'));
+            Set<Service> serviceList = provider.getServices();
+            for (Service service : serviceList) {
+                types.add(service.getType());
+                System.out.println("\t" + service.getType() + ": " + service.getAlgorithm());
+            }
+        }
+        
+        System.out.println("\n\nTypes: " + types);
+    }
 }
