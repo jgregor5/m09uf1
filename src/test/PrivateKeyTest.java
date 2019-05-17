a1a58a0402aec4e4c7dca4504be582aa4dea48bd
julian <julian.gregorio@adhocware.com>
2019-05-17 13:40:31 +0200

    compare message size

--- src/test/PrivateKeyTest.java
+++ src/test/PrivateKeyTest.java
@@ -13,7 +13,18 @@ public class PrivateKeyTest {
     
     public static void main(String[] args) throws Exception {
         
-        String text = "Under canopy frost white in winter there'll be buds forming, waiting to greet the spring";
+        String result;
+        
+        String text1 = "Under canopy frost white in winter there'll be buds forming, waiting to greet the spring";
+        String text2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        
+        result = test(text1);
+        System.out.println("result: " + result);
+        result = test(text2);
+        System.out.println("result: " + result);
+    }
+    
+    public static String test(String text) throws Exception {
         
         KeyGenerator keyGen = KeyGenerator.getInstance("DES");
         keyGen.init(56);
@@ -21,9 +32,9 @@ public class PrivateKeyTest {
         
         byte[] encrypted = encrypt(secretKey, text.getBytes("UTF-8"));        
         byte[] decrypted = decrypt(secretKey, encrypted);
-        
-        String result = new String(decrypted, "UTF-8");
-        System.out.println(result);
+                
+        String result = new String(decrypted, "UTF-8");        
+        return result;
     }
  
     public static byte[] encrypt(Key secretKey, byte[] data) throws Exception {
--- src/test/PublicKeyTest.java
+++ src/test/PublicKeyTest.java
@@ -12,10 +12,26 @@ import javax.crypto.Cipher;
  * @author julian
  */
 public class PublicKeyTest {
-
+    
     public static void main(String[] args) throws Exception {
+        
+        String result;
+        
+        String text1 = "Under canopy frost white in winter there'll be buds forming, waiting to greet the spring";
+        String text2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        
+        result = test(text1, true);
+        System.out.println("result pub/pri: " + result);
+        result = test(text1, false);
+        System.out.println("result pri/pub: " + result);
+        
+        result = test(text2, true);
+        System.out.println("result pub/pri: " + result);
+        result = test(text2, false);
+        System.out.println("result pri/pub: " + result);
+    }
 
-        String text = "Under canopy frost white in winter there'll be buds forming, waiting to greet the spring";
+    private static String test(String text, boolean publicPrivate) throws Exception {
 
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
         kpg.initialize(2048);
@@ -26,17 +42,17 @@ public class PublicKeyTest {
         
         byte[] encrypted, decrypted;
         
-        encrypted = encrypt(publicKey, text.getBytes("UTF-8"));
-        decrypted = decrypt(privateKey, encrypted);
+        if (publicPrivate) {
+            encrypted = encrypt(publicKey, text.getBytes("UTF-8"));
+            decrypted = decrypt(privateKey, encrypted);
+        }
+        else {
+            encrypted = encrypt(privateKey, text.getBytes("UTF-8"));
+            decrypted = decrypt(publicKey, encrypted);
+        }
 
-        String resultPubPri = new String(decrypted, "UTF-8");
-        System.out.println(resultPubPri);
-        
-        encrypted = encrypt(privateKey, text.getBytes("UTF-8"));
-        decrypted = decrypt(publicKey, encrypted);
-
-        String resultPriPub = new String(decrypted, "UTF-8");
-        System.out.println(resultPriPub);
+        String result = new String(decrypted, "UTF-8");        
+        return result;
     }
 
     public static byte[] encrypt(Key secretKey, byte[] data) throws Exception {
